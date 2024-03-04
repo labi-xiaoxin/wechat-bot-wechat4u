452eabfbd1aa8221af318b9142a01a98c883acf7
wangyiping <47637410@qq.com>
2024-03-04 16:33:06 +0800

    修复部分数据异步存在问题

--- src/core/sendMessage.js
+++ src/core/sendMessage.js
@@ -11,33 +11,46 @@ import { WxPushData } from "../../wx-push-data.js"
  * @param message 消息对象
  */
 export async function sendMessage(message) {
+    const MyMessage = {
+        type: await message.type(),
+        self: await message.self(),
+        text: await message.text(),
+        room: await message.room(),
+        mentionSelf: await message.mentionSelf(),
+        roomName: (await message.room()?.topic()) || null,
+        alias: await message.talker().alias(),
+        date: await message.date(),
+        talkerId: await message.payload.talkerId,
+        listenerId: await message.payload.listenerId == undefined ? null : message.payload.listenerId,
+        roomId: await message.payload.roomId == undefined ? null : message.payload.roomId
+    }
     //1、判断消息是否符合逻辑
-    var checkResult = checkMessage(message);
+    var checkResult = checkMessage(MyMessage);
     if (!checkResult) {
         return
     }
     //2、发送后端处理消息,并返回发送微信
-    forwardMsg(message);
+    forwardMsg(MyMessage,message);
 }
 
 /**
  * 判断消息是否符合逻辑
  * 
- * @param {Message} message 消息 
+ * @param {MyMessage} message 消息 
  * @returns 符合逻辑返回true；否则返回false
  */
 function checkMessage(message) {
     //消息类型不是文本
-    if (message.type() != MessageType.MESSAGE_TYPE_TEXT) {
+    if (message.type != MessageType.MESSAGE_TYPE_TEXT) {
         return false
     }
     //自己发送的消息不处理
-    if (message.self()) {
+    if (message.self) {
         return false
     }
     //引用的文本不处理
     const regexp = /「[^」]+」\n- - - - - - - - - - - - - -/;
-    if (regexp.test(message.text())) {
+    if (regexp.test(message.text)) {
         return false
     }
     //非白名单内的不处理
@@ -54,14 +67,14 @@ function checkMessage(message) {
  *      是私聊且在白名单内，则返回2
  *      否则返回0
  * 
- * @param {Message} message 消息内容
+ * @param {MyMessage} message 消息内容
  */
-async function isRoomOrPrivate(message) {
+function isRoomOrPrivate(message) {
     //房间内的消息需要@ 且群聊在名单内
-    if (message.room() != null && await message.mentionSelf() == true && MYCONFIG.roomWhiteList.includes((await room?.topic()) || null)) {
+    if (message.room != null && message.mentionSelf == true && MYCONFIG.roomWhiteList.includes(message.roomName)) {
         return 1;
     }//非房间内消息，且发送人备注在名单内
-    else if (message.room() == null && MYCONFIG.aliasWhiteList.includes(await message.talker().alias())) {
+    else if (message.room == null && MYCONFIG.aliasWhiteList.includes(message.alias)) {
         return 2;
     } else {
         return 0;
@@ -72,12 +85,12 @@ async function isRoomOrPrivate(message) {
  * 发送后端处理消息，并返回发送微信
  * @param {Message} message 消息对象
  */
-async function forwardMsg(message) {
-    log.info(`\n 消息发送时间:${message.date()} 
-    消息发送人:${message.talker()} 
-    消息类型:${message.type()} 
-    消息是否@我:${await message.mentionSelf()} 
-    消息内容:${message.text()} `)
+async function forwardMsg(MyMessage,message) {
+    log.info(`\n 消息发送时间:${MyMessage.date} 
+    消息发送人:${MyMessage.alias} 
+    消息类型:${MyMessage.type} 
+    消息是否@我:${MyMessage.mentionSelf} 
+    消息内容:${MyMessage.text} `)
 
     //1、简单返回消息
     // sendSay(message,"你好");
@@ -92,21 +105,21 @@ async function forwardMsg(message) {
         data: JSON.stringify(
             new WxPushData(
                 //消息
-                message.text(),
+                MyMessage.text,
                 //消息发送人备注
-                await message.talker().alias(),
+                MyMessage.alias,
                 //消息发送者ID 微信ID不是永久性
-                message.payload.talkerId,
+                MyMessage.talkerId,
                 //私聊才有listenerID
-                message.payload.listenerId == undefined ? null : message.payload.listenerId,
+                MyMessage.listenerId,
                 //群聊才有房间ID
-                message.payload.roomId == undefined ? null : message.payload.roomId))
+                MyMessage.roomId))
     }).then(result => {
         var reMsg = result.data.msg;
-        sendSay(message, reMsg);
+        sendSay(message, reMsg,MyMessage);
     }).catch(response => {
         log.error(`异常响应：${response}`);
-        sendSay(message, `异常响应:${response}`);
+        sendSay(message, `异常响应:${response}`,MyMessage);
         return `异常响应：${responese}`;
     })
 }
@@ -118,8 +131,8 @@ async function forwardMsg(message) {
  * @param {Message} message 消息内容
  * @param {String} reStr 回复内容
  */
-async function sendSay(message, reStr) {
-    const isROP = await isRoomOrPrivate(message);
+function sendSay(message, reStr, MyMessage) {
+    const isROP = isRoomOrPrivate(MyMessage);
     //房间内消息
     if (isROP == 1) {
         message.room().say(`${(reStr)}\n @by ${MYCONFIG.robotName}`, message.talker())
